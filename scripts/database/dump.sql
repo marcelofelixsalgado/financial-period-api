f6561455a7ccfc70a5f596f81aa245cda9eb1c67
Marcelo Felix Salgado <marcelo-felix-luizalabs@WMEC-P2CCPXX3FC.local>
2022-12-15 15:00:21 -0300

    Including the database init script on source control

---
+++ scripts/database/dump.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE IF NOT EXISTS financial_db;
+USE financial_db;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS user_credentials;
+DROP TABLE IF EXISTS periods;
+
+CREATE TABLE users(
+    id varchar(255) primary key,
+    name varchar(255) not null,
+    phone varchar(50) not null,
+    email varchar(255) not null,
+    created_at datetime not null,
+    updated_at datetime,
+
+    CONSTRAINT UC_Phone UNIQUE (phone),
+    CONSTRAINT UC_Email UNIQUE (email)
+);
+
+CREATE TABLE user_credentials(
+    id varchar(255) primary key,
+    user_id varchar(255) not null,
+    password varchar(255) not null,
+    created_at datetime not null,
+    updated_at datetime,
+
+    CONSTRAINT FK_UserCredentials FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE periods(
+    id varchar(255) primary key,
+    code varchar(50) not null,
+    name varchar(255) not null,
+    year varchar(4) not null,
+    start_date datetime not null,
+    end_date datetime not null,
+    created_at datetime not null,
+    updated_at datetime
+);
+
+
